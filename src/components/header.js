756cfeae4d214e525e3fd931ece09eb664f8679f
John Rock <johnmrock.jr@gmail.com>
2019-12-10 21:51:28 -0500

    add container styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,24 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header className="large-container">
+      <div className="site-logo">
+        <Link to="/">John<span>Rock</span></Link>
+      </div>
+      <nav id="swup" className="nav" role="menu">
+          <div className="nav-sub" role="menuitem">
+            <Link to={`/`}>Home</Link>
+          </div>
+          <div className="nav-sub" role="menuitem">
+            <Link to={`/`}>Portfolio</Link>
+          </div>
+          <div className="nav-sub" role="menuitem">
+            <Link to={`/`}>Blog</Link>
+          </div>
+          <div className="nav-sub" role="menuitem">
+            <Link to={`/`}>Contact</Link>
+          </div>
+      </nav>
   </header>
 )
 
