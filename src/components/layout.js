76aa95b91d7d3c75e851e8879a5fd0c200e64f84
John Rock <johnmrock.jr@gmail.com>
2019-12-11 12:57:21 -0500

    add social media buttons

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { FaLinkedinIn, FaGithub, FaCodepen, FaRegEnvelope } from "react-icons/fa";
 import Header from "./header"
 import MainNav from "./mainNav"
 import "./layout.css"
@@ -32,6 +33,12 @@ const Layout = ({ children }) => {
           <main className="page-content">{children}</main>
           <footer className="site-foot">
           <MainNav />
+          <div className="bottom-social">
+            <a href="https://www.linkedin.com/in/john-rock/" target="_blank"><FaLinkedinIn /></a>
+            <a href="https://github.com/john-rock" target="_blank"><FaGithub /></a>
+            <a href="/https://codepen.io/Jrock2394" target="_blank"><FaCodepen /></a>
+            <a href="mailto:johnmrock.jr@gmail.com" target="_blank"><FaRegEnvelope /></a>
+          </div>
             <p>Crafted in Northeast Pennsylvania</p>
             <p>© {new Date().getFullYear()} John Rock</p>
           </footer>
