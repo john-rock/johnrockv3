51ac0ee9008e6e3fa7131493754127ad057845dd
John Rock <johnmrock.jr@gmail.com>
2019-12-14 11:46:05 -0500

    add hidden form field

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -25,6 +25,7 @@ const Contact = () => {
         data-netlify="true"
         action="/thank-you"
       >
+        <input type="hidden" name="form-name" value="Contact Form" />
         <p class="hidden">
           <label>
             Don’t fill this out if you're human: <input name="bot-field" />
